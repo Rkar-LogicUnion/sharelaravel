a4160b253e3d6f5deed50659723d4e156fad920b
Rkar-LogicUnion <appleidrkar@icloud.com>
2021-06-12 00:28:33 +0630

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Testing Share
                 </div>
 
                 <div class="links">
